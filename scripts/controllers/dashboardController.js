0e90212a3e4acafc26e5dde1400fc0efa03c3d1d
chubwa <tuzoengelbert@gmail.com>
2016-01-22 21:12:38 +0300

    Updating dashboard card

--- scripts/controllers/dashboardController.js
+++ scripts/controllers/dashboardController.js
@@ -25,7 +25,9 @@ dashboardController.controller('DashboardController',['$scope','dashboardsManage
             $scope.dashboardItems = dashboard.dashboardItems;
            angular.forEach($scope.dashboardItems,function(value){
                 value.column_size = $scope.getColumnSize(value.shape);
+                value.labelCard=$scope.getCardSize(value.shape);
             })
+            console.log($scope.dashboardItems);
             $scope.loading=false;
         });
         //$scope.column_size
@@ -43,21 +45,37 @@ dashboardController.controller('DashboardController',['$scope','dashboardsManage
         };
         $scope.cardClassResizable=function(shapeSize,dashboardItem){
             var size=shapeSize.split("-").pop();
+            var labelCard='';
             var sizeCol='';
             var sizeName='';
             if(size==12){
                 sizeCol='col-md-4';
                 sizeName="NORMAL";
+                labelCard='Small';
             }else if(size==8){
                 sizeCol='col-md-12';
                 sizeName="FULL_WIDTH";
+                labelCard='Large';
             }else if(size==4){
                 sizeCol='col-md-8';
                 sizeName="DOUBLE_WIDTH";
+                labelCard='Medium';
             }
               console.log(sizeName)
             dashboardItem.column_size =$scope.getColumnSize(sizeName);
+            dashboardItem.labelCard =labelCard;
         }
+        $scope.getCardSize=function(shapeSize){
+            var labelCard='';
+            if(angular.lowercase(shapeSize)=="double_width") {
+                labelCard='Medium';
+            }else if(angular.lowercase(shapeSize)=="full_width"){
+                labelCard='Large';
+            }else if(angular.lowercase(shapeSize)=="normal") {
+                labelCard='Small';
+            }
+            return labelCard;
+         }
         $scope.getDashboardItem = function(dashboardItem) {
             return dashboardItem[dashboardItem.type];
         }
