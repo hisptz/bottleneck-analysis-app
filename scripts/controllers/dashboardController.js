bada363946b02f2bc689eb4b5265e6ef55bce013
Kelvin Mbwilo <kelvinmbwilo@gmail.com>
2016-01-22 21:12:50 +0300

    creating analytics object

--- scripts/controllers/dashboardController.js
+++ scripts/controllers/dashboardController.js
@@ -1,3 +1,4 @@
+
 var dashboardController  = angular.module('dashboardController',[]);
 dashboardController.controller('DashboardController',['$scope','dashboardsManager','dashboardItemsManager',
     '$routeParams','$modal','$timeout','$translate','$anchorScroll','Paginator','ContextMenuSelectedItem',
@@ -22,10 +23,14 @@ dashboardController.controller('DashboardController',['$scope','dashboardsManage
 
         $scope.loading = true;
         dashboardsManager.getDashboard($routeParams.dashboardid).then(function(dashboard){
+
             $scope.dashboardItems = dashboard.dashboardItems;
+            console.log($scope.dashboardItems);
            angular.forEach($scope.dashboardItems,function(value){
                 value.column_size = $scope.getColumnSize(value.shape);
-            })
+               $scope.getAnalytics(value, 408, false )
+
+            });
             $scope.loading=false;
         });
         //$scope.column_size
@@ -60,6 +65,83 @@ dashboardController.controller('DashboardController',['$scope','dashboardsManage
         }
         $scope.getDashboardItem = function(dashboardItem) {
             return dashboardItem[dashboardItem.type];
+        }
+
+        $scope.getAnalytics = function( dashboardItem, width, prepend )
+        {
+            console.log(dashboardItem.type);
+            width = width || 408;
+            prepend = prepend || false;
+
+            var graphStyle = "width:" + width + "px; overflow:hidden;";
+            var tableStyle = "width:" + width + "px;";
+            var userOrgUnit =  [];
+
+            if ( "CHART" == dashboardItem.type && dashboardItem.chart.id !== 'BFi8AtKeIkU' )
+            {
+
+                console.log(DHIS.getChart({
+                    url: '../../../',
+                    el: 'plugin-' + dashboardItem.id,
+                    id: dashboardItem.chart.id,
+                    width: width,
+                    height: 308,
+                    dashboard: true,
+                    crossDomain: false,
+                    skipMask: true,
+                    userOrgUnit: userOrgUnit,
+                    domainAxisStyle: {
+                        labelRotation: 45,
+                        labelFont: '10px sans-serif',
+                        labelColor: '#111'
+                    },
+                    rangeAxisStyle: {
+                        labelFont: '9px sans-serif'
+                    },
+                    legendStyle: {
+                        labelFont: 'normal 10px sans-serif',
+                        labelColor: '#222',
+                        labelMarkerSize: 10,
+                        titleFont: 'bold 12px sans-serif',
+                        titleColor: '#333'
+                    },
+                    seriesStyle: {
+                        labelColor: '#333',
+                        labelFont: '9px sans-serif'
+                    }
+                }));
+            }
+            else if ( "MAP" == dashboardItem.type )
+            {
+
+                DHIS.getMap({
+                    url: '../../../',
+                    el: 'plugin-' + dashboardItem.id,
+                    id: dashboardItem.map.id,
+                    hideLegend: true,
+                    dashboard: true,
+                    crossDomain: false,
+                    skipMask: true,
+                    mapViews: [{
+                        userOrgUnit: userOrgUnit
+                    }]
+                });
+            }
+            else if ( "REPORT_TABLE" == dashboardItem.type )
+            {
+
+                DHIS.getTable({
+                    url: '../../../',
+                    el: 'plugin-' + dashboardItem.id,
+                    id: dashboardItem.reportTable.id,
+                    dashboard: true,
+                    crossDomain: false,
+                    skipMask: true,
+                    displayDensity: 'compact',
+                    fontSize: 'small',
+                    userOrgUnit: userOrgUnit
+                });
+            }
         }
 
     }])
