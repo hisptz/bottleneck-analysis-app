6f9d7c9e4703e085d0daf26930d9619a18e96365
Barnabas Makonda <barnabasmakonda@gmail.com>
2018-02-17 10:18:07 +0300

    new branch to fix issues

--- src/app/modules/map/store/selectors/index.ts
+++ src/app/modules/map/store/selectors/index.ts
@@ -1,4 +1,3 @@
-export * from './leaflet-layers.selectors';
 export * from './visualization-object.selectors';
 export * from './legend-set.selectors';
 export * from './visualization-legend.selectors';
