b472e22b5aa952c984c18f7d440263483318c6a8
MAKOSCAFEE <barnabasmakonda@gmail.com>
2018-03-09 10:04:49 +0300

    added style-component

---
+++ src/app/modules/map/components/map-style/map-style.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-style',
+  templateUrl: './map-style.component.html',
+  styleUrls: ['./map-style.component.css']
+})
+export class MapStyleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
