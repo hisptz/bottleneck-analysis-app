fea71de852884d6051c6888c9e2450149064feac
Barnabas Makonda <barnabasmakonda@gmail.com>
2018-04-10 13:46:16 +0300

    added selection for class and color

--- src/app/modules/map/components/map-style/map-style.component.ts
+++ src/app/modules/map/components/map-style/map-style.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { colorBrewer } from '../../utils/colorBrewer';
 
 @Component({
   selector: 'app-map-style',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-style.component.css']
 })
 export class MapStyleComponent implements OnInit {
+  classifications = ['Equal interval', 'Equal counts'];
+  classes = [3, 4, 5, 6, 7, 8, 9];
+  default_color = 'PuBu';
+  default_class = 3;
+  colors = Object.keys(colorBrewer);
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onChange(deviceValue) {
+    console.log(deviceValue);
   }
 
+  getColors(key, classSize) {
+    return colorBrewer[key][classSize];
+  }
 }
