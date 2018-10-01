8606608cd2b12e34dcc2ea3e9ddb9cadd163ce1c
Rajab Mkomwa <rajeyw@gmail.com>
2018-10-01 22:44:33 +0300

    feat(sharing): Added support to filter dashboard based on sharing

--- src/app/dashboard/helpers/index.ts
+++ src/app/dashboard/helpers/index.ts
@@ -5,3 +5,4 @@ export * from './get-current-dashboard-id.helper';
 export * from './get-dashboard-bookmark-status.helper';
 export * from './get-dashboard-items-from-dashboards.helpers';
 export * from './get-standardized-dashboard-visualization.helper';
+export * from './get-filtered-dashboards-based-on-sharing.helper';
