18b1d92185d96bbd03d1872fb1c14e8003bf39a5
Rajab Mkomwa <rajeyw@gmail.com>
2019-01-17 17:06:32 +0300

    feat(index-db): Added index db support when loading organisation unit groups

--- src/app/dashboard/modules/ngx-dhis2-data-selection-filter/modules/ngx-dhis2-org-unit-filter/services/org-unit-group.service.ts
+++ src/app/dashboard/modules/ngx-dhis2-data-selection-filter/modules/ngx-dhis2-org-unit-filter/services/org-unit-group.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, catchError, switchMap, tap } from 'rxjs/operators';
 import { NgxDhis2HttpClientService } from '@hisptz/ngx-dhis2-http-client';
 import { OrgUnitGroup } from '../models';
+import { AppDatabaseService } from 'src/app/services/app-database.service';
 
 @Injectable({ providedIn: 'root' })
 export class OrgUnitGroupService {
-  constructor(private httpClient: NgxDhis2HttpClientService) {}
+  constructor(
+    private httpClient: NgxDhis2HttpClientService,
+    private appDatabaseService: AppDatabaseService
+  ) {}
 
-  loadAll(): Observable<OrgUnitGroup[]> {
+  loadFromServer() {
     return this.httpClient
       .get(`organisationUnitGroups.json?fields=id,name&paging=false`)
       .pipe(map((res: any) => res.organisationUnitGroups || []));
+  }
+
+  loadAll(): Observable<OrgUnitGroup[]> {
+    return this.appDatabaseService.getAll('organisationUnitGroups').pipe(
+      catchError(() => of([])),
+      switchMap((orgUnitGroups: OrgUnitGroup[]) =>
+        orgUnitGroups.length > 0
+          ? of(orgUnitGroups)
+          : this.loadFromServer().pipe(
+              tap((orgUnitGroupsFromServer: OrgUnitGroup[]) => {
+                this.appDatabaseService
+                  .saveBulk('organisationUnitGroups', orgUnitGroupsFromServer)
+                  .subscribe(() => {});
+              })
+            )
+      )
+    );
   }
 }
