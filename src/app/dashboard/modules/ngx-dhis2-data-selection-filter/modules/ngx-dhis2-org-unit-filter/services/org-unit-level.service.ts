7f1776f0408af2ef58b993a18ef2c73e5a83e637
Rajab Mkomwa <rajeyw@gmail.com>
2019-01-17 16:49:59 +0300

    feat(index-db): Added index db support when loading organisation unit levels

--- src/app/dashboard/modules/ngx-dhis2-data-selection-filter/modules/ngx-dhis2-org-unit-filter/services/org-unit-level.service.ts
+++ src/app/dashboard/modules/ngx-dhis2-data-selection-filter/modules/ngx-dhis2-org-unit-filter/services/org-unit-level.service.ts
@@ -1,18 +1,45 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import Dexie from 'dexie';
+import { map, catchError, switchMap, tap } from 'rxjs/operators';
 import { NgxDhis2HttpClientService } from '@hisptz/ngx-dhis2-http-client';
 import { OrgUnitLevel } from '../models';
+import { AppDatabaseService } from 'src/app/services/app-database.service';
 
 @Injectable({ providedIn: 'root' })
 export class OrgUnitLevelService {
-  constructor(private httpClient: NgxDhis2HttpClientService) {}
+  orgUnitLevelDB: Dexie.Table<OrgUnitLevel, string>;
+  constructor(
+    private httpClient: NgxDhis2HttpClientService,
+    private appDatabaseService: AppDatabaseService
+  ) {
+    this.orgUnitLevelDB = this.appDatabaseService.table(
+      'organisationUnitLevels'
+    );
+  }
 
-  loadAll(): Observable<OrgUnitLevel[]> {
+  loadFromServer() {
     return this.httpClient
       .get(
         `organisationUnitLevels.json?fields=id,name,level&paging=false&order=level:asc`
       )
       .pipe(map((res: any) => res.organisationUnitLevels || []));
+  }
+
+  loadAll(): Observable<OrgUnitLevel[]> {
+    return this.appDatabaseService.getAll('organisationUnitLevels').pipe(
+      catchError(() => of([])),
+      switchMap((orgUnitLevels: OrgUnitLevel[]) =>
+        orgUnitLevels.length > 0
+          ? of(orgUnitLevels)
+          : this.loadFromServer().pipe(
+              tap((orgUnitLevelsFromServer: OrgUnitLevel[]) => {
+                this.appDatabaseService
+                  .saveBulk('organisationUnitLevels', orgUnitLevelsFromServer)
+                  .subscribe(() => {});
+              })
+            )
+      )
+    );
   }
 }
--- src/app/services/app-database.service.ts
+++ src/app/services/app-database.service.ts
@@ -1,14 +1,50 @@
 import { Injectable } from '@angular/core';
 import Dexie from 'dexie';
 import { OrgUnit } from '../dashboard/modules/ngx-dhis2-data-selection-filter/modules/ngx-dhis2-org-unit-filter/models';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppDatabaseService extends Dexie {
-  organisationUnits: Dexie.Table<OrgUnit, string>;
   constructor() {
     super('dashboard');
-    this.version(1).stores({ organisationUnits: 'id,name' });
+    this.version(1).stores({
+      organisationUnits: 'id,name',
+      organisationUnitLevels: 'id,name',
+      organisationUnitGroups: 'id,name'
+    });
+  }
+
+  getAll(schemaName: string): Observable<any> {
+    return new Observable(observer => {
+      this.table(schemaName)
+        .toArray()
+        .then(
+          (orgUnits: OrgUnit[]) => {
+            observer.next(orgUnits);
+            observer.complete();
+          },
+          (error: any) => {
+            observer.next(error);
+          }
+        );
+    });
+  }
+
+  saveBulk(schemaName: string, data: any[]): Observable<any[]> {
+    return new Observable(observer => {
+      this.table(schemaName)
+        .bulkPut(data)
+        .then(
+          () => {
+            observer.next(data);
+            observer.complete();
+          },
+          error => {
+            observer.error(error);
+          }
+        );
+    });
   }
 }
