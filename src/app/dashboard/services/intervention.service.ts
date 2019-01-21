3583306b9217a6af9da6fc87ffc7dee704124df6
Rajab Mkomwa <rajeyw@gmail.com>
2019-01-21 19:04:25 +0300

    feat(default-intervention): Added support to create default interventions for new instances

--- src/app/dashboard/services/intervention.service.ts
+++ src/app/dashboard/services/intervention.service.ts
@@ -4,6 +4,7 @@ import { NgxDhis2HttpClientService } from '@hisptz/ngx-dhis2-http-client';
 import { switchMap, catchError, map } from 'rxjs/operators';
 import { forkJoin, of } from 'rxjs';
 import { Intervention } from '../models/intervention.model';
+import { getSanitizedInterventions } from '../helpers';
 
 @Injectable({
   providedIn: 'root'
@@ -23,15 +24,36 @@ export class InterventionService {
           )
         )
       ),
-      catchError(() => of([]))
+      catchError((error: any) =>
+        error.status !== 404 ? of([]) : this.generatePredifinedInterventions()
+      )
     );
   }
 
-  createIntervention(intervention: Intervention) {
-    return this.http.post(
-      `${this.dataStoreUrl}/${intervention.id}`,
-      intervention
+  generatePredifinedInterventions() {
+    return this.loadPredefinedInterventions().pipe(
+      switchMap((interventions: any[]) => {
+        return forkJoin(
+          _.map(
+            getSanitizedInterventions(interventions),
+            (sanitizedIntervention: Intervention) =>
+              this.createIntervention(sanitizedIntervention)
+          )
+        );
+      })
     );
+  }
+
+  loadPredefinedInterventions() {
+    return this.http
+      .get('config/default-interventions.json', { useRootUrl: true })
+      .pipe(catchError(() => of([])));
+  }
+
+  createIntervention(intervention: Intervention) {
+    return this.http
+      .post(`${this.dataStoreUrl}/${intervention.id}`, intervention)
+      .pipe(map(() => intervention));
   }
 
   updateIntervention(intervention: Intervention) {
