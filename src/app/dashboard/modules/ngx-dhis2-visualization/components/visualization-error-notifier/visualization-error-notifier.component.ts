11b60d1e129caa4a8171b8501fddf72902d55ddf
Rajab Mkomwa <rajeyw@gmail.com>
2019-02-25 00:02:00 +0300

    feat: add goof error message when widget could not be found

--- src/app/dashboard/modules/ngx-dhis2-visualization/components/visualization-error-notifier/visualization-error-notifier.component.ts
+++ src/app/dashboard/modules/ngx-dhis2-visualization/components/visualization-error-notifier/visualization-error-notifier.component.ts
@@ -3,14 +3,15 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'visualization-error-notifier',
   templateUrl: './visualization-error-notifier.component.html',
-  styleUrls: ['./visualization-error-notifier.component.css'],
+  styleUrls: ['./visualization-error-notifier.component.css']
 })
 export class VisualizationErrorNotifierComponent implements OnInit {
-
   @Input() errorMessage: any;
-  constructor() { }
-
-  ngOnInit() {
+  @Input() titleMessage: string;
+  constructor() {
+    this.titleMessage =
+      'Aw, Snap. We are sorry for this inconvenience, our server tells us that';
   }
 
+  ngOnInit() {}
 }
