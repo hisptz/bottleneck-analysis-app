affd77d72d666235c154ba3a114e5b06d680bf81
Walter V. Faustine <waltervfaustine@gmail.com>
2019-04-23 14:40:33 +0300

    feat(map.component.ts): add visualization support for geographical accessibility intervation on map
    Add support to show the visualition on map by showing only the data that comes from the Geographical
    Accessbility
    
    fix #106

--- src/app/dashboard/modules/ngx-dhis2-visualization/modules/map/containers/map/map.component.ts
+++ src/app/dashboard/modules/ngx-dhis2-visualization/modules/map/containers/map/map.component.ts
@@ -6,6 +6,8 @@ import * as _ from 'lodash';
 import * as fromUtils from '../../utils';
 import { VisualizationObject } from '../../models/visualization-object.model';
 import { getSplitedVisualizationLayers } from '../../../../helpers';
+import { visitSiblingRenderNodes } from '@angular/core/src/view/util';
+import { sanitizeResourceUrl } from '@angular/core/src/sanitization/sanitization';
 
 @Component({
   selector: 'app-map',
@@ -29,6 +31,8 @@ export class MapComponent implements OnInit {
   ngOnInit() {
     this.store.dispatch(new fromStore.InitiealizeVisualizationLegend(this.id));
 
+    // console.log("RAJABU: " + JSON.stringify(this.visualizationLayers))
+
     this.transformVisualizationObject(this.visualizationConfig, this.visualizationLayers, this.id);
     this.visualizationObject$ = this.store.select(fromStore.getCurrentVisualizationObject(this.id));
   }
@@ -45,14 +49,61 @@ export class MapComponent implements OnInit {
       const newAnalytics = { ...analytics, rows };
       return { ...vizLayer, analytics: newAnalytics };
     });
-    this.displayConfigurations = { ...this.visualizationUiConfig, ...this.visualizationLayers[0].config };
-    const layers = getSplitedVisualizationLayers(visualizationConfig.type, cleanedOutLayers);
-    const { visObject } = fromUtils.transformVisualizationObject(visualizationConfig, layers, id);
-    this.visualizationObject = {
-      ...this.visualizationObject,
-      componentId: this.id,
-      ...visObject
-    };
+
+    if (visualizationConfig.currentType === 'MAP') {
+      let analytics;
+
+        this.displayConfigurations = { ...this.visualizationUiConfig, ...this.visualizationLayers[0].config };
+        const layersMetadata = getSplitedVisualizationLayers(visualizationConfig.type, cleanedOutLayers);
+        const { visObject } = fromUtils.transformVisualizationObject(visualizationConfig, layersMetadata, id);
+
+        const crudVisualizationObject = {
+          ...this.visualizationObject,
+          componentId: this.id,
+          ...visObject
+        };
+
+        const analyticsKeys = Object.keys(crudVisualizationObject.analytics)
+        const layersUIDs = []
+        const geoFeatureSanitizedAnalytics = {}
+        analyticsKeys.map((analyticsKey) => {
+          if (analyticsKey) {
+            if (crudVisualizationObject.analytics[analyticsKey].rows.length != 0) {
+              crudVisualizationObject.analytics[analyticsKey].rows.map((rowData) => {
+                if (rowData[4] === 'k1ZoaGAFVL3') {
+                  geoFeatureSanitizedAnalytics[analyticsKey.toString()] = crudVisualizationObject.analytics[analyticsKey]
+                  analytics = geoFeatureSanitizedAnalytics;
+                  const newAnalytics = { ...crudVisualizationObject, analytics };
+                  newAnalytics.layers.filter((layer) => {
+                    if (layer.id === analyticsKey) {
+                      if (!layersUIDs.includes(analyticsKey)) {
+                        layersUIDs.push(analyticsKey)
+                      }
+                    }
+                  })
+                } else {
+                  console.log('[MAP Analytics] No Row Data Matching That UID')
+                }
+              });
+            } else {
+  
+            }
+          } else {
+            console.log('[MAP Analytics] Missing analytics in the Visualization Object.')
+          }
+        });
+        analytics = geoFeatureSanitizedAnalytics;
+        const newAnalytics = { ...crudVisualizationObject, analytics };
+        const layers = newAnalytics.layers.filter((layer) => { 
+          return layersUIDs.includes(layer.id) 
+        }).map((layer, pos) => { 
+          (pos === 0) ? layer.visible = true : layer.visible = false; return layer 
+        })
+        const sanitizedVisualizationObject = { ...crudVisualizationObject, layers }
+        this.visualizationObject = sanitizedVisualizationObject
+    } else {
+      console.log('[MAP Visualization] Visualization type is not MAP')
+    }
     this.store.dispatch(new fromStore.AddVisualizationObjectComplete(this.visualizationObject));
   }
 
