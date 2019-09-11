d57aa1697b7d1cf7f23670116e1a8066c389f42e
Rajab Mkomwa <rajeyw@gmail.com>
2019-09-11 15:25:38 +0300

    feat: add support to fetch root cause widget data ids
    Add support to fetch root causes data ids so as to show which inteventions have root cause analysis
    data

--- src/app/dashboard/containers/current-dashboard/current-dashboard.component.ts
+++ src/app/dashboard/containers/current-dashboard/current-dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { take } from 'rxjs/operators';
+import { take, first } from 'rxjs/operators';
 
 // root state
 import { State } from '../../../store/reducers';
@@ -29,6 +29,11 @@ import {
   getDashboardMenuHeight
 } from '../../store/selectors';
 import { User } from '@iapps/ngx-dhis2-http-client';
+import { VisualizationDataSelection } from '../../modules/ngx-dhis2-visualization/models';
+import {
+  getCurrentGlobalDataSelections,
+  getGlobalDataSelectionSummary
+} from '../../store/selectors/data-selections.selectors';
 
 @Component({
   selector: 'app-current-dashboard',
@@ -101,11 +106,11 @@ export class CurrentDashboardComponent implements OnInit {
     );
 
     this.currentGlobalDataSelections$ = this.store.select(
-      fromDashboardSelectors.getCurrentGlobalDataSelections(false)
+      getCurrentGlobalDataSelections(false)
     );
 
     this.currentGlobalDataSelectionSummary$ = this.store.select(
-      fromDashboardSelectors.getGlobalDataSelectionSummary
+      getGlobalDataSelectionSummary
     );
 
     this.progressMessages$ = this.store.select(
--- src/app/store/effects/index.ts
+++ src/app/store/effects/index.ts
@@ -3,11 +3,13 @@ import { SystemInfoEffects } from './system-info.effects';
 import { UserEffects } from './user.effects';
 import { LegendSetEffects } from './legend-set.effects';
 import { DataGroupEffects } from './data-group.effects';
+import { RootCauseDataEffects } from './root-cause-data.effects';
 
 export const effects: any[] = [
   RouterEffects,
   SystemInfoEffects,
   UserEffects,
   LegendSetEffects,
-  DataGroupEffects
+  DataGroupEffects,
+  RootCauseDataEffects
 ];
