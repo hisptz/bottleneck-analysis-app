2398e585edb1ea31ede5579bc348f713c5c265c6
Rajab Mkomwa <rajeyw@gmail.com>
2020-01-07 16:49:53 +0300

    refactor: remove OR search support to enable 100 match for search name
    Remove OR search in data selection so to allow 100 match for the searched item

--- src/app/shared/pipes/filter-by-name.pipe.ts
+++ src/app/shared/pipes/filter-by-name.pipe.ts
@@ -1,17 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filterByName'
+  name: 'filterByName',
 })
 export class FilterByNamePipe implements PipeTransform {
   transform(list: any[], name: any): any {
-    const splittedName = name ? name.split(/[\.\-_,; ]/) : [];
-    return splittedName.length > 0
-      ? list.filter((item: any) =>
-          splittedName.some(
-            (nameString: string) =>
-              item.name.toLowerCase().indexOf(nameString.toLowerCase()) !== -1
-          )
+    return name
+      ? list.filter(
+          (item: any) =>
+            (item ? item.name : '')
+              .toLowerCase()
+              .indexOf(name.toLowerCase()) !== -1
         )
       : list;
   }
