337d148077acb977b824dccdfdd2ed5a5b3df401
Rajab Mkomwa <rajeyw@gmail.com>
2020-01-07 20:57:07 +0300

    refactor: remove support to save indicators and function in indexDB

--- src/app/dashboard/modules/ngx-dhis2-data-selection-filter/modules/data-filter/services/function.service.ts
+++ src/app/dashboard/modules/ngx-dhis2-data-selection-filter/modules/data-filter/services/function.service.ts
@@ -2,22 +2,16 @@ import { Injectable } from '@angular/core';
 import { NgxDhis2HttpClientService } from '@iapps/ngx-dhis2-http-client';
 import * as _ from 'lodash';
 import { of, zip } from 'rxjs';
-import { catchError, mergeMap, switchMap, tap } from 'rxjs/operators';
-import { AppDatabaseService } from 'src/app/services/app-database.service';
-
-import { FunctionObject } from '../models';
+import { catchError, mergeMap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class FunctionService {
   private _dataStoreUrl: string;
-  constructor(
-    private http: NgxDhis2HttpClientService,
-    private appDatabaseService: AppDatabaseService
-  ) {
+  constructor(private http: NgxDhis2HttpClientService) {
     this._dataStoreUrl = 'dataStore/functions';
   }
 
-  private _loadFromServer() {
+  loadAll() {
     return this.http.get(this._dataStoreUrl).pipe(
       mergeMap((functionIds: Array<string>) =>
         zip(
@@ -25,23 +19,6 @@ export class FunctionService {
         ).pipe(catchError(() => of([])))
       ),
       catchError(() => of([]))
-    );
-  }
-
-  loadAll() {
-    return this.appDatabaseService.getAll('functions').pipe(
-      catchError(() => of([])),
-      switchMap((functions: FunctionObject[]) =>
-        functions.length > 0
-          ? of(functions)
-          : this._loadFromServer().pipe(
-              tap((functionsFromServer: FunctionObject[]) => {
-                this.appDatabaseService
-                  .saveBulk('functions', functionsFromServer)
-                  .subscribe(() => {});
-              })
-            )
-      )
     );
   }
 
