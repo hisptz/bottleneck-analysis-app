8353cb09f54e535e1dcfb6111782c7c48f611272
Danford Kija <kijadanford@gmail.com>
2021-11-02 12:53:03 +0300

    add table framwork component

---
+++ src/modules/Dashboard/components/RootCauseAnalysis/components/rootCauseTable/rootCauseTable.tsx
@@ -0,0 +1,72 @@
+import i18n from "@dhis2/d2-i18n";
+import { DataTable, TableHead, DataTableRow, DataTableColumnHeader, TableBody, DataTableCell, TableFoot, Button, IconMore16 } from "@dhis2/ui";
+import React from "react";
+import "./rootCauseTable.css";
+
+export default function RootCausseTable() {
+  return (
+    <DataTable>
+      <TableHead>
+        <DataTableRow>
+          <DataTableColumnHeader>SN.</DataTableColumnHeader>
+          <DataTableColumnHeader>OrgUnit</DataTableColumnHeader>
+          <DataTableColumnHeader>Period</DataTableColumnHeader>
+          <DataTableColumnHeader>Intervention</DataTableColumnHeader>
+          <DataTableColumnHeader>Bottleneck</DataTableColumnHeader>
+          <DataTableColumnHeader>Indicator</DataTableColumnHeader>
+          <DataTableColumnHeader>Possible root cause</DataTableColumnHeader>
+          <DataTableColumnHeader>Possible solution</DataTableColumnHeader>
+          <DataTableColumnHeader>Action</DataTableColumnHeader>
+        </DataTableRow>
+      </TableHead>
+      <TableBody>
+        <DataTableRow>
+          <DataTableCell bordered>1</DataTableCell>
+          <DataTableCell bordered>Animal Region</DataTableCell>
+          <DataTableCell bordered>2019</DataTableCell>
+          <DataTableCell bordered>Focused ANC coverage</DataTableCell>
+          <DataTableCell bordered>Continouss Utilisation</DataTableCell>
+          <DataTableCell bordered>Continous Utilization ANC Visits</DataTableCell>
+          <DataTableCell bordered>Lack of human resources</DataTableCell>
+          <DataTableCell bordered>Hire and train new midwives</DataTableCell>
+          <DataTableCell bordered>
+            <Button icon={<IconMore16 color="#212529" />} name="Icon large button" small value="default" />
+          </DataTableCell>
+        </DataTableRow>
+        <DataTableRow>
+          <DataTableCell bordered>2</DataTableCell>
+          <DataTableCell bordered>Animal Region</DataTableCell>
+          <DataTableCell bordered>2019</DataTableCell>
+          <DataTableCell bordered>Focused ANC coverage</DataTableCell>
+          <DataTableCell bordered>Continouss Utilisation</DataTableCell>
+          <DataTableCell bordered>Continous Utilization ANC Visits</DataTableCell>
+          <DataTableCell bordered>Lack of human resources</DataTableCell>
+          <DataTableCell bordered>Hire and train new midwives</DataTableCell>
+          <DataTableCell bordered>
+            <Button icon={<IconMore16 color="#212529" />} name="Icon large button" small value="default" />
+          </DataTableCell>
+        </DataTableRow>
+        <DataTableRow>
+          <DataTableCell bordered>3</DataTableCell>
+          <DataTableCell bordered>Animal Region</DataTableCell>
+          <DataTableCell bordered>2019</DataTableCell>
+          <DataTableCell bordered>Focused ANC coverage</DataTableCell>
+          <DataTableCell bordered>Continouss Utilisation</DataTableCell>
+          <DataTableCell bordered>Continous Utilization ANC Visits</DataTableCell>
+          <DataTableCell bordered>Lack of human resources</DataTableCell>
+          <DataTableCell bordered>Hire and train new midwives</DataTableCell>
+          <DataTableCell bordered>
+            <Button icon={<IconMore16 color="#212529" />} name="Icon large button" small value="default" />
+          </DataTableCell>
+        </DataTableRow>
+      </TableBody>
+      <TableFoot>
+        <DataTableRow>
+          <DataTableCell align={"end"} colSpan="9">
+            <Button>{i18n.t("Add New")}</Button>
+          </DataTableCell>
+        </DataTableRow>
+      </TableFoot>
+    </DataTable>
+  );
+}
--- src/modules/Dashboard/components/RootCauseAnalysis/index.tsx
+++ src/modules/Dashboard/components/RootCauseAnalysis/index.tsx
@@ -1,7 +1,12 @@
 import i18n from "@dhis2/d2-i18n";
 import React from "react";
 import DashboardCard from "../DashboardCard";
+import RootCauseTable from "../RootCauseAnalysis/components/rootCauseTable/rootCauseTable";
 
 export default function RootCauseAnalysis() {
-  return <DashboardCard title={i18n.t("Root Cause Analysis")}>Root Kijacode here</DashboardCard>;
+  return (
+    <DashboardCard title={i18n.t("Root Cause Analysis")}>
+      <RootCauseTable />
+    </DashboardCard>
+  );
 }
