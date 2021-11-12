176b20bd58a2831f3daabfbf3169330c223a33a5
Gift Nnko <nnkogift@gmail.com>
2021-11-12 15:39:28 +0300

    fix(*): - Implemented chart analytics query logic

--- src/modules/Intervention/components/AnalysisChart/state/data.ts
+++ src/modules/Intervention/components/AnalysisChart/state/data.ts
@@ -1,15 +1,17 @@
 import { selectorFamily } from "recoil";
+import { EngineState } from "../../../../../core/state/dataEngine";
 import { DataItems, OrgUnit, Period } from "../../SubLevelAnalysis/state/dimensions";
+import { getChartAnalytics } from "../services/getChartAnalytics";
 
 export const ChartData = selectorFamily({
   key: "chart-data",
   get:
     (id: string) =>
     async ({ get }) => {
+      const engine = get(EngineState);
       const period = get(Period(id));
       const orgUnits = get(OrgUnit(id));
-      const data = get(DataItems(id));
-      console.log({ data, period, orgUnits });
-      return {};
+      const dataItems = get(DataItems(id));
+      return await getChartAnalytics({ dx: dataItems, ou: orgUnits, pe: period }, engine);
     },
 });
