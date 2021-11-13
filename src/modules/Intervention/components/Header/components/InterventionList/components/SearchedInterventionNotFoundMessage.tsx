67c1aae745b7fdcdbeb394fb8a71b47510b7b95b
Gift Nnko <nnkogift@gmail.com>
2021-11-13 10:31:21 +0300

    fix(Search Intervention Message): Improvements
    - Added styling and icon to search error message

--- src/modules/Intervention/components/Header/components/InterventionList/components/SearchedInterventionNotFoundMessage.tsx
+++ src/modules/Intervention/components/Header/components/InterventionList/components/SearchedInterventionNotFoundMessage.tsx
@@ -1,10 +1,12 @@
 import i18n from "@dhis2/d2-i18n";
+import { colors, IconError24 } from "@dhis2/ui";
 import React from "react";
 
 export default function SearchedInterventionNotFoundMessage() {
   return (
-    <div className="column w-100" style={{ maxWidth: "100%", paddingTop: "1vh" }}>
-      <div className="row w-100">{i18n.t("Searched Intervention is not found")}</div>
+    <div className="row w-100 gap align-center" style={{ maxWidth: "100%", paddingTop: "1vh" }}>
+      <IconError24 color={colors.grey700} />
+      <p style={{ color: colors.grey700, margin: 0 }}>{i18n.t("Searched Intervention is not found")}</p>
     </div>
   );
 }
