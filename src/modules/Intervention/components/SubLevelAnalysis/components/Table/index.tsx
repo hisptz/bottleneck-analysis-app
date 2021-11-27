9816fc32bc9b02af2d7b938f4e53cdbbb3ce479d
Gift Nnko <nnkogift@hotmail.com>
2021-11-27 10:04:58 +0300

    feat(SubLevelAnalysis): Implementation
    - Implemented download excel functionality for sub level analysis table

--- src/modules/Intervention/components/SubLevelAnalysis/components/Table/index.tsx
+++ src/modules/Intervention/components/SubLevelAnalysis/components/Table/index.tsx
@@ -1,9 +1,13 @@
-import { Button, DataTable } from "@dhis2/ui";
-import React, { Suspense, useMemo } from "react";
+import i18n from "@dhis2/d2-i18n";
+import { Button, ButtonStrip, DataTable } from "@dhis2/ui";
+import React, { Suspense, useMemo, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import useScreenDimension from "../../../../../../core/hooks/useScreenDimension";
 import CardLoader from "../../../../../../shared/components/loaders/CardLoader";
+import { getExcelFileFromAnalyticsData } from "../../../../../../shared/utils/download";
+import { InterventionStateSelector } from "../../../../state/intervention";
+import { InterventionOrgUnitState, InterventionPeriodState } from "../../../../state/selections";
 import { normalTableLayout, switchedTableLayout } from "../../constants/tableLayouts";
 import { TableConfig, TableLayout } from "../../state/layout";
 import TableBody from "./components/TableBody";
@@ -13,8 +17,13 @@ import classes from "./Table.module.css";
 export default function Table() {
   const { id } = useParams<{ id: string }>();
   const switchLayout = useSetRecoilState(TableLayout(id));
+  const interventionName = useRecoilValue<string>(InterventionStateSelector({ id, path: ["name"] }));
+  const { name: periodName } = useRecoilValue(InterventionPeriodState(id));
+  const { displayName: orgUnitName } = useRecoilValue(InterventionOrgUnitState(id));
   const { width } = useRecoilValue(TableConfig(id));
   const { width: screenWidth } = useScreenDimension();
+
+  const tableRef = useRef(null);
   const onLayoutChange = () => {
     switchLayout((prevLayout) => {
       if (prevLayout.columns.includes("dx")) {
@@ -23,6 +32,10 @@ export default function Table() {
         return switchedTableLayout;
       }
     });
+  };
+
+  const onDownloadExcel = () => {
+    getExcelFileFromAnalyticsData(tableRef, `${interventionName}_${orgUnitName}_${periodName}`);
   };
 
   const tableWidth = useMemo(() => {
@@ -36,9 +49,12 @@ export default function Table() {
     <Suspense fallback={<CardLoader />}>
       <div className="column">
         <div className="row end p-8">
-          <Button onClick={onLayoutChange}>Switch Layout</Button>
+          <ButtonStrip>
+            <Button onClick={onLayoutChange}>{i18n.t("Switch Layout")}</Button>
+            <Button onClick={onDownloadExcel}>{i18n.t("Download Excel")}</Button>
+          </ButtonStrip>
         </div>
-        <DataTable fixed width={`${tableWidth}px`} scrollWidth={"5000px"} scrollHeight={"500px"} className={classes["table"]} bordered>
+        <DataTable ref={tableRef} fixed width={`${tableWidth}px`} scrollWidth={"5000px"} scrollHeight={"500px"} className={classes["table"]} bordered>
           <TableHeader />
           <TableBody />
         </DataTable>
---
+++ src/shared/utils/download.ts
@@ -0,0 +1,8 @@
+import { utils as xlsx, writeFile } from "xlsx";
+
+export function getExcelFileFromAnalyticsData(tableRef: any, title: string) {
+  const sheet = xlsx.table_to_sheet(tableRef.current);
+  const workbook = xlsx.book_new();
+  xlsx.book_append_sheet(workbook, sheet, `${title.substring(0, 31)}`); //TODO: Notify user if title is too long
+  writeFile(workbook, `${title}.${"xlsx"}`);
+}
