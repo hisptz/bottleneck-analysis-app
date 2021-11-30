492a4a596c6c66dcf32eefd6c05f4ff0826eed61
chingaloEric <echingalo@gmail.com>
2021-11-30 17:34:58 +0300

    refactor(rootcauseanalysis): added a request id for refetching

--- src/modules/Intervention/components/RootCauseAnalysis/state/data.ts
+++ src/modules/Intervention/components/RootCauseAnalysis/state/data.ts
@@ -1,10 +1,15 @@
 import { filter, flattenDeep } from "lodash";
-import { selectorFamily } from "recoil";
+import { atom, selectorFamily } from "recoil";
 import { EngineState } from "../../../../../core/state/dataEngine";
 import { UserOrganisationUnits } from "../../../../../core/state/user";
 import { PeriodSelection } from "../../../../../shared/interfaces/interventionConfig";
 import { InterventionStateSelector } from "../../../state/intervention";
 import { getRootCausesData } from "../services/data";
+
+export const RootCauseDataRequestId = atom({
+  key: "root-cause-config-request-id",
+  default: 0,
+});
 
 export const RootCauseData = selectorFamily({
   key: "root-cause-analysis-data",
@@ -12,6 +17,7 @@ export const RootCauseData = selectorFamily({
     (id: string) =>
     async ({ get }) => {
       const engine = get(EngineState);
+      get(RootCauseDataRequestId);
       const periodSelection: PeriodSelection = get(
         InterventionStateSelector({
           id,
