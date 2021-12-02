74e1485112c33115ab3f1aba268526f12b08510e
chingaloEric <echingalo@gmail.com>
2021-12-02 18:54:58 +0300

    refactor(rootcauseactions): added a component for root cause table actions
    the actions inclue delete and edit action items

--- src/modules/Intervention/components/RootCauseAnalysis/components/RootCauseTable/index.tsx
+++ src/modules/Intervention/components/RootCauseAnalysis/components/RootCauseTable/index.tsx
@@ -1,12 +1,13 @@
 import i18n from "@dhis2/d2-i18n";
-import { Button, DataTable, DataTableCell, DataTableRow, IconMore24, IconEdit24, IconDelete24, Menu, MenuItem, Popover, TableBody, TableFoot } from "@dhis2/ui";
+import { Button, DataTable, DataTableCell, DataTableRow, TableBody, TableFoot } from "@dhis2/ui";
 import { find } from "lodash";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import "./rootCauseTable.css";
 import { useParams } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { RootCauseTableConfig } from "../../state/config";
 import { RootCauseData, RootCauseDataRequestId } from "../../state/data";
+import RootCauseActionsProps from "./components/RootCauseActionsComponent";
 import RootCauseFormComponent from "./components/RootCauseFormComponent";
 import RootCauseTableHeaderComponent from "./components/RootCauseTableHeaderComponent";
 import classes from "./RootCauseTable.module.css";
@@ -16,31 +17,35 @@ export default function RootCauseTable({ tableRef }: { tableRef: any }) {
   const [rootCauseDataRequestId, setRootCauseDataRequestId] = useRecoilState(RootCauseDataRequestId);
   const { columns, rows, rowIds } = useRecoilValue(RootCauseTableConfig(id));
   const rootCauseData = useRecoilValue(RootCauseData(id));
-  const ref = useRef<HTMLDivElement | null>(null);
-  const [stateRef, setStateRef] = useState<any>();
   const [rootCauseFormDisplayStatus, setRootCauseFormDisplayStatus] = useState(false);
+  const [selectedRootCauseData, setSelectedRootCauseData] = useState<any>({});
 
   function onUpdateRootCauseFormDisplayStatus() {
     setRootCauseFormDisplayStatus(!rootCauseFormDisplayStatus);
   }
 
   async function onDeleteRootCause(rootCauseIndex: number) {
+    setSelectedRootCauseData({});
     const rootCauseId = rowIds[rootCauseIndex];
-    console.log(rootCauseId);
+    console.log("onDeleteRootCause", rootCauseId);
   }
 
   async function onUpdateRootCause(rootCauseIndex: number) {
-    const rootCause = rootCauseData[rootCauseIndex];
-    console.log(rootCause);
+    const rootCause: any = rootCauseData[rootCauseIndex];
+    const data = { ...rootCause.dataValues, id: rootCause.id };
+    setSelectedRootCauseData(data);
+    onUpdateRootCauseFormDisplayStatus();
   }
 
   function onSaveRootCauseSuccessfully() {
     onUpdateRootCauseFormDisplayStatus();
     setRootCauseDataRequestId(rootCauseDataRequestId + 1);
+    setSelectedRootCauseData({});
   }
 
   function onSaveRootCauseFailed() {
     // Error exists
+    setSelectedRootCauseData({});
   }
 
   return (
@@ -48,38 +53,16 @@ export default function RootCauseTable({ tableRef }: { tableRef: any }) {
       <DataTable className={classes["table"]} bordered>
         <RootCauseTableHeaderComponent columns={columns} />
         <TableBody>
-          {rows.map((row, index) => (
-            <DataTableRow key={index}>
+          {rows.map((row, rowIndex) => (
+            <DataTableRow key={rowIndex}>
               {row.map(({ key, value }, index) => {
                 if (key === "Actions") {
                   return (
                     <DataTableCell className={classes["table-cell"]} key={index} align="center">
-                      <Button
-                        className={classes["button"]}
-                        onClick={(_: any, e: MouseEvent) => {
-                          setStateRef(e.target);
-                          ref.current?.scrollIntoView({ behavior: "smooth" });
-                        }}>
-                        <IconMore24 />
-                      </Button>
-                      {/* <Popover onClickOutside={() => setStateRef(undefined)} placement="left-start" reference={stateRef}>
-                        <Menu>
-                          <MenuItem
-                            onClick={() => {
-                              onUpdateRootCause(index);
-                            }}
-                            label={i18n.t("Edit")}
-                            icon={<IconEdit24 />}
-                          />
-                          <MenuItem
-                            onClick={() => {
-                              onDeleteRootCause(index);
-                            }}
-                            label={i18n.t("Delete")}
-                            icon={<IconDelete24 />}
-                          />
-                        </Menu>
-                      </Popover> */}
+                      <RootCauseActionsProps
+                        key={index}
+                        onUpdateRootCause={() => onUpdateRootCause(rowIndex)}
+                        onDeleteRootCause={() => onDeleteRootCause(rowIndex)}></RootCauseActionsProps>
                     </DataTableCell>
                   );
                 }
@@ -106,7 +89,7 @@ export default function RootCauseTable({ tableRef }: { tableRef: any }) {
         hideModal={rootCauseFormDisplayStatus}
         onSavingError={onSaveRootCauseFailed}
         onCancelForm={onUpdateRootCauseFormDisplayStatus}
-        rootCauseData={{}}
+        rootCauseData={selectedRootCauseData}
         onSuccessfullySaveRootCause={onSaveRootCauseSuccessfully}></RootCauseFormComponent>
     </div>
   );
