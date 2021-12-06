4d985d4cae236a6ff5a4d09faf2f860a9922ca64
Gift Nnko <nnkogift@hotmail.com>
2021-12-06 09:25:46 +0300

    fix(SelectedIndicator): Fixes
     - Unselect indicator on Clear all

--- src/modules/InterventionConfiguration/components/Determinants/components/DeterminantArea/index.tsx
+++ src/modules/InterventionConfiguration/components/Determinants/components/DeterminantArea/index.tsx
@@ -1,35 +1,44 @@
 import i18n from "@dhis2/d2-i18n";
 import { Button } from "@dhis2/ui";
+import { cloneDeep, isEmpty, reduce } from "lodash";
 import React from "react";
-import GroupDeterminantComponent from "./component";
 import "./DeterminantArea.css";
-import { useSetRecoilState } from "recoil";
-import { InterventionDirtySelector } from "../../../../state/data";
 import { useParams } from "react-router-dom";
-import { cloneDeep } from "lodash";
+import { useRecoilCallback, useRecoilValue } from "recoil";
+import { InterventionDirtySelector } from "../../../../state/data";
+import { SelectedDeterminantId, SelectedIndicatorId } from "../../../../state/edit";
+import GroupDeterminantComponent from "./component";
 
 export default function DeterminantArea(): React.ReactElement {
   const { id } = useParams<{ id: string }>();
-  const setDeterminants = useSetRecoilState(InterventionDirtySelector({ id, path: ["dataSelection", "groups"] }));
-
-  const onClearAll = () => {
-    if (window.confirm("Are you sure you want to clear all indicators?")) {
-      setDeterminants((prevState: any) => {
-        const newGroups = cloneDeep(prevState);
-        newGroups.forEach((group: any) => {
-          group.items = [];
-        });
-        return newGroups;
-      });
-    }
-  };
+  const determinants = useRecoilValue(InterventionDirtySelector({ id, path: ["dataSelection", "groups"] }));
+  const allDeterminantsEmpty: boolean = reduce(determinants, (acc, determinant) => acc && isEmpty(determinant.items), true as boolean);
+  const onClearAll = useRecoilCallback(
+    ({ set, reset }) =>
+      () => {
+        if (window.confirm("Are you sure you want to clear all indicators?")) {
+          set(InterventionDirtySelector({ id, path: ["dataSelection", "groups"] }), (prevState: any) => {
+            const newGroups = cloneDeep(prevState);
+            newGroups.forEach((group: any) => {
+              group.items = [];
+            });
+            return newGroups;
+          });
+          reset(SelectedIndicatorId(id));
+          reset(SelectedDeterminantId(id));
+        }
+      },
+    [id]
+  );
 
   return (
     <div className="determinant-container">
       <div className="determinant-main">
         <div className="determinant-main-header">
           <h3>{i18n.t("Determinants")}</h3>
-          <Button onClick={onClearAll}>{i18n.t("Clear All")}</Button>
+          <Button disabled={allDeterminantsEmpty} onClick={onClearAll}>
+            {i18n.t("Clear All")}
+          </Button>
         </div>
         <div className={"determinant-selector"}>
           <GroupDeterminantComponent />
