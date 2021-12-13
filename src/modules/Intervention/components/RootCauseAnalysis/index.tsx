502eea30cee1f74e49619f75d3d2c188a1256bfc
Gift Nnko <nnkogift@hotmail.com>
2021-12-13 09:55:59 +0300

    fix(Root Cause Table): Fixes & Improvements
    - Modified card title to include period and orgUnit

--- src/modules/Intervention/components/RootCauseAnalysis/index.tsx
+++ src/modules/Intervention/components/RootCauseAnalysis/index.tsx
@@ -1,11 +1,12 @@
 import i18n from "@dhis2/d2-i18n";
-import { IconDownload24 } from "@dhis2/ui";
+import { colors, IconDownload24 } from "@dhis2/ui";
 import React, { useRef } from "react";
 import { useFullScreenHandle } from "react-full-screen";
 import { useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { downloadExcelFromTable } from "../../../../shared/utils/download";
 import { InterventionStateSelector } from "../../state/intervention";
+import { InterventionOrgUnitState, InterventionPeriodState } from "../../state/selections";
 import InterventionCard from "../Card";
 import RootCauseTable from "./components/RootCauseTable";
 
@@ -13,7 +14,8 @@ export default function RootCauseAnalysis() {
   const { id } = useParams<{ id: string }>();
   const handle = useFullScreenHandle();
   const interventionName = useRecoilValue<string>(InterventionStateSelector({ id, path: ["name"] })) ?? "";
-
+  const orgUnit = useRecoilValue(InterventionOrgUnitState(id));
+  const period = useRecoilValue(InterventionPeriodState(id));
   const tableRef = useRef(null);
 
   const onDownload = () => {
@@ -31,7 +33,12 @@ export default function RootCauseAnalysis() {
           icon: <IconDownload24 />,
         },
       ]}
-      title={i18n.t("Root Cause Analysis")}>
+      title={
+        <div className="row" style={{ gap: 8 }}>
+          <h4>{i18n.t("Root Cause Analysis")}: </h4>
+          <h4 style={{ color: colors.grey700 }}>{`${orgUnit.displayName ?? ""} - ${period.name ?? ""} `}</h4>
+        </div>
+      }>
       <RootCauseTable tableRef={tableRef} />
     </InterventionCard>
   );
