48db4bd765ccd4f9d14d3246726ca0fb131639bc
Gift Nnko <nnkogift@hotmail.com>
2021-12-15 11:29:26 +0300

    feat(Auto Migration): Implementation
    - Implemented auto migration of configuration when the app is opened for
    the first time

--- src/modules/Landing/hooks/index.ts
+++ src/modules/Landing/hooks/index.ts
@@ -1,15 +1,22 @@
+import { useSetting } from "@dhis2/app-service-datastore";
 import { head, isEmpty } from "lodash";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { useRecoilValue } from "recoil";
+import { DATA_MIGRATION_CHECK } from "../../../constants/dataStore";
 import { InterventionSummary } from "../../../core/state/intervention";
 import { InterventionSummary as InterventionSummaryType } from "../../../shared/interfaces/interventionConfig";
 
 export function useAppInit(): void {
   const history = useHistory();
   const interventionSummary = useRecoilValue(InterventionSummary);
+  const [migration] = useSetting(DATA_MIGRATION_CHECK, { global: true });
   useEffect(() => {
     function navigate() {
+      if (!migration) {
+        history.replace("/migrate");
+        return;
+      }
       if (interventionSummary && !isEmpty(interventionSummary)) {
         const firstIntervention: InterventionSummaryType | undefined = head(interventionSummary);
         if (firstIntervention) {
@@ -21,7 +28,6 @@ export function useAppInit(): void {
         history.replace("/no-interventions");
       }
     }
-
     navigate();
-  }, [history, interventionSummary]);
+  }, [history, interventionSummary, migration]);
 }
