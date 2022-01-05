7a22211fdcd877b54e8455826ea53922938db6ec
Gift Nnko <nnkogift@hotmail.com>
2022-01-05 16:52:46 +0300

    fix(Archive Access): Fixes
    - Implemented access control for archives. Access is now controlled as
    per the intervention configuration within the archive

--- src/modules/Archives/state/data.ts
+++ src/modules/Archives/state/data.ts
@@ -1,15 +1,23 @@
-import { orderBy } from "lodash";
+import { filter, orderBy } from "lodash";
 import { selector, selectorFamily } from "recoil";
+import { getUserAuthority } from "../../../core/services/user";
 import { EngineState } from "../../../core/state/dataEngine";
+import { UserState } from "../../../core/state/user";
 import { Archive as ArchiveType } from "../../../shared/interfaces/archive";
+import { createInterventionSummary } from "../../../shared/services/interventionSummary";
 import { getArchive, getArchives } from "../services/data";
 
 export const Archives = selector<Array<ArchiveType>>({
   key: "archives-state",
   get: async ({ get }) => {
     const engine = get(EngineState);
+    const user = get(UserState);
     const archives = (await getArchives(engine)) as unknown as Array<ArchiveType>;
-    return orderBy(archives, [(archive) => new Date(archive.dateCreated), "config.name"], "desc");
+    const filteredArchives: Array<ArchiveType> = filter(archives, (archive) => {
+      const { read } = getUserAuthority(user, createInterventionSummary(archive?.config));
+      return read;
+    }) as Array<ArchiveType>;
+    return orderBy(filteredArchives, [(archive) => new Date(archive.dateCreated), "config.name"], "desc");
   },
 });
 
