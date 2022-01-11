0720fe2440dc5c26df5572effd1a2293f52eace7
Gift Nnko <nnkogift@hotmail.com>
2022-01-11 09:44:32 +0300

    feat(Determinants Configuration): Implementation
    - Implemented use short names as labels feature

--- src/modules/InterventionConfiguration/components/Determinants/components/DeterminantArea/index.tsx
+++ src/modules/InterventionConfiguration/components/Determinants/components/DeterminantArea/index.tsx
@@ -1,11 +1,12 @@
 import i18n from "@dhis2/d2-i18n";
 import { Button, CheckboxField } from "@dhis2/ui";
 import { cloneDeep, isEmpty, reduce } from "lodash";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback } from "react";
 import "./DeterminantArea.css";
 import { useFormContext } from "react-hook-form";
 import { useParams } from "react-router-dom";
 import { useRecoilState } from "recoil";
+import { DataItem } from "../../../../../../shared/interfaces/interventionConfig";
 import { UseShortName } from "../../../../state/edit";
 import GroupDeterminantComponent from "./component/Determinants";
 
@@ -17,7 +18,7 @@ export default function DeterminantArea(): React.ReactElement {
   const allDeterminantsEmpty: boolean = reduce(determinants, (acc, determinant) => acc && isEmpty(determinant?.items), true as boolean);
 
   const onClearAll = useCallback(() => {
-    if (window.confirm("Are you sure you want to clear all indicators?")) {
+    if (window.confirm(i18n.t("Are you sure you want to clear all indicators?"))) {
       const determinants = getValues("groups");
       const newGroups = cloneDeep(determinants);
       newGroups.forEach((group: any) => {
@@ -26,6 +27,30 @@ export default function DeterminantArea(): React.ReactElement {
       setValue("groups", newGroups);
     }
   }, [getValues, setValue]);
+
+  const setShortNameAsLabels = useCallback(() => {
+    const determinants = getValues("groups");
+    const newGroups = cloneDeep(determinants);
+    newGroups.forEach((group: any) => {
+      group.items.forEach((item: DataItem) => {
+        item.label = item?.shortName ?? item?.name;
+      });
+    });
+    setValue("groups", newGroups);
+  }, [getValues, setValue]);
+  const onUseShortNameChange = useCallback(
+    ({ checked }: { checked: boolean }) => {
+      if (checked) {
+        if (window.confirm(i18n.t("This will replace all indicator labels with their short names. Proceed?"))) {
+          setUseShortName(true);
+          setShortNameAsLabels();
+        }
+      } else {
+        setUseShortName(false);
+      }
+    },
+    [setShortNameAsLabels, setUseShortName]
+  );
 
   return (
     <div className="determinant-container">
@@ -37,12 +62,7 @@ export default function DeterminantArea(): React.ReactElement {
           </Button>
         </div>
         <div style={{ padding: "8px 16px" }}>
-          <CheckboxField
-            disabled={allDeterminantsEmpty}
-            checked={useShortName}
-            onChange={({ checked }: { checked: boolean }) => setUseShortName(checked)}
-            label={i18n.t("Use short names as labels")}
-          />
+          <CheckboxField disabled={allDeterminantsEmpty} checked={useShortName} onChange={onUseShortNameChange} label={i18n.t("Use short names as labels")} />
         </div>
         <div className={"determinant-selector"}>
           <GroupDeterminantComponent />
