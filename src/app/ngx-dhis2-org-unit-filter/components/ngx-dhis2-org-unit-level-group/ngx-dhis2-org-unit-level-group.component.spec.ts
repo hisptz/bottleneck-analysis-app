4168032fcb4389d369d705e6007987c461edca06
Joseph Chingalo <profschingalo@gmail.com>
2022-01-13 17:13:03 +0300

    fix: 🐛 Reference of pipe for ou filter module

--- src/app/ngx-dhis2-org-unit-filter/components/ngx-dhis2-org-unit-level-group/ngx-dhis2-org-unit-level-group.component.spec.ts
+++ src/app/ngx-dhis2-org-unit-filter/components/ngx-dhis2-org-unit-level-group/ngx-dhis2-org-unit-level-group.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { NgxDhis2OrgUnitLevelGroupComponent } from "./ngx-dhis2-org-unit-level-group.component";
 import { NgxDhis2OrgUnitProgressComponent } from "../ngx-dhis2-org-unit-progress/ngx-dhis2-org-unit-progress.component";
-import { FilterByOrgUnitGroupLevelPipe } from "../../pipes";
+import { FilterByOrgUnitGroupLevelPipe } from "../../pipes/org-unit-group-level-filter.pipe";
 
 describe("NgxDhis2OrgUnitLevelGroupComponent", () => {
   let component: NgxDhis2OrgUnitLevelGroupComponent;
