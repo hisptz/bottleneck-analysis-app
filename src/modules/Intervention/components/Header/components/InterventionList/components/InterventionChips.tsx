d07cef445d65698d169948693305118966e58ab9
Danford Kija <kijadanford@gmail.com>
2022-01-19 17:53:32 +0300

    fix(chart-tooltip): on hover                   - add values on display tooltip                   - style tooltip                   - refix bottom space on tooltip container                   - add opt to display name or short name

--- src/modules/Intervention/components/Header/components/InterventionList/components/InterventionChips.tsx
+++ src/modules/Intervention/components/Header/components/InterventionList/components/InterventionChips.tsx
@@ -1,5 +1,5 @@
 import { Chip, IconStarFilled24 } from "@dhis2/ui";
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { UserState } from "../../../../../../../core/state/user";
@@ -24,10 +24,15 @@ export default function InterventionChips({ showAll, interventions }: Interventi
     [history, id]
   );
 
+  useEffect(() => {
+    const el = document.getElementById(id);
+    console.log("sgatus ", el);
+  }, [document.getElementById("check-wrap")]);
   return (
     <div className="column w-100" style={{ maxWidth: "100%" }}>
       <div
-        className="row w-100 "
+        className="row w-100"
+        id="check-wrap"
         style={{ maxWidth: "100%", overflowX: "hidden", flexWrap: "wrap", height: showAll ? 120 : 38, overflowY: showAll ? "auto" : "hidden" }}>
         {interventions?.map((intervention) => (
           <Chip
