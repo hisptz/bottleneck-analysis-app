ea62ee2af29726c614e01487f62e154c673206db
Gift Nnko <nnkogift@hotmail.com>
2022-01-31 12:52:49 +0300

    feat(package.json): Dependency
    - Added react-reaflet
    
    Signed-off-by: Gift Nnko <nnkogift@hotmail.com>

--- src/modules/Intervention/components/SubLevelAnalysis/components/Map/index.tsx
+++ src/modules/Intervention/components/SubLevelAnalysis/components/Map/index.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 export default function Map() {
-  return <div> Map</div>;
+  return (
+    <>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
+      </Helmet>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </>
+  );
 }
