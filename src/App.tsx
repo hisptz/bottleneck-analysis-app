b8135131477b9733eb2efcda098e258204cdc838
Gift Nnko <nnkogift@hotmail.com>
2022-01-31 15:50:44 +0300

    feat(Map): Implementation
    - initial map setup
    
    Signed-off-by: Gift Nnko <nnkogift@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,12 @@ const MyApp = (): React.ReactElement => {
         <ConfirmDialogProvider>
           <Suspense fallback={<InitialAppLoader />}>
             <Helmet>
+              <link
+                rel="stylesheet"
+                href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+                integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+                crossOrigin=""
+              />
               <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" />
             </Helmet>
             <Router />
--- src/modules/Intervention/components/SubLevelAnalysis/components/Map/index.tsx
+++ src/modules/Intervention/components/SubLevelAnalysis/components/Map/index.tsx
@@ -5,15 +5,7 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 export default function Map() {
   return (
     <>
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossOrigin=""
-        />
-      </Helmet>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer style={{ height: "100%", width: "100%" }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
