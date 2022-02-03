e692f97251aa8588daa0178e24e71f94ca629589
Gift Nnko <nnkogift@hotmail.com>
2022-02-03 22:02:06 +0300

    refactor(Map): Refactor
    - Deleted useMapData hook and implemented useBoundaryData hook
    
    Signed-off-by: Gift Nnko <nnkogift@hotmail.com>

--- src/modules/Intervention/components/SubLevelAnalysis/components/Map/index.tsx
+++ src/modules/Intervention/components/SubLevelAnalysis/components/Map/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { LayersControl, MapContainer, TileLayer } from "react-leaflet";
 import BoundaryLayer from "./components/BoundaryLayer";
+import useBoundaryData from "./components/BoundaryLayer/hooks/data";
 import ThematicLayer from "./components/ThematicLayer";
-import useMapData from "./hooks/useMapData";
 
 export default function Map() {
-  const { center, bounds } = useMapData();
+  const { center, bounds } = useBoundaryData();
   return (
     <>
       <MapContainer bounds={bounds} bounceAtZoomLimits center={center} style={{ height: "100%", width: "100%", minHeight: 900 }} scrollWheelZoom={false}>
