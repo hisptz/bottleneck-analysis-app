469e6380d88b121fc463e1e0cec037403fa97ade
Gift Nnko <nnkogift@hotmail.com>
2022-02-03 23:18:47 +0300

    feat(Map): Choropleth
    - Implemented Bubble layer
    
    Signed-off-by: Gift Nnko <nnkogift@hotmail.com>

---
+++ src/modules/Intervention/components/SubLevelAnalysis/components/Map/components/ThematicLayer/components/IndicatorLayer/components/Choropleth/index.tsx
@@ -0,0 +1,42 @@
+import { colors } from "@dhis2/ui";
+import { head } from "lodash";
+import React from "react";
+import { Polygon, Popup, Tooltip } from "react-leaflet";
+import { getColorFromLegendSet, highlightFeature, resetHighlight } from "../../../../../../utils/map";
+
+const defaultStyle = {
+  weight: 1,
+};
+const highlightStyle = {
+  weight: 2,
+};
+
+export default function Choropleth({ indicator, data }: { indicator: any; data: any }) {
+  return (
+    <Polygon
+      eventHandlers={{ mouseover: (e) => highlightFeature(e, highlightStyle), mouseout: (e) => resetHighlight(e, defaultStyle) }}
+      pathOptions={{
+        fillColor: getColorFromLegendSet(head(indicator?.legendSets), data.data),
+        fillOpacity: 1,
+        color: colors.grey900,
+        weight: 1,
+      }}
+      key={`${data.indicator.id}-layer`}
+      positions={data.orgUnit.co}>
+      <Tooltip>
+        {data?.orgUnit?.name} ({data?.data})
+      </Tooltip>
+      <Popup minWidth={80}>
+        <h3>{data?.orgUnit?.name}</h3>
+        <div>
+          <b>Level: </b>
+          {data?.orgUnit?.level}
+        </div>
+        <div>
+          <b>{indicator?.displayName}: </b>
+          {data?.data}
+        </div>
+      </Popup>
+    </Polygon>
+  );
+}
