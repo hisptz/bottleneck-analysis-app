806c8d6858f25c08418c4fba04ec548f8cb2db86
Gift Nnko <nnkogift@hotmail.com>
2022-04-13 15:39:52 +0300

    :sparkles: feat(legends config): validations implementation
    - Implemented legends validation function

--- src/modules/InterventionConfiguration/utils/validators.ts
+++ src/modules/InterventionConfiguration/utils/validators.ts
@@ -1,5 +1,18 @@
-import { InterventionConfig } from "../../../shared/interfaces/interventionConfig";
+import { InterventionConfig, Legend } from "../../../shared/interfaces/interventionConfig";
+import { sortBy } from "lodash";
 
 export function validate(interventionConfig: InterventionConfig): boolean {
   return Boolean(interventionConfig.name);
+}
+
+
+export function validateLegends(legends: Legend[]): boolean {
+  const sortedLegends = sortBy(legends, "endValue");
+
+  return sortedLegends.every((legend, index) => {
+    if (index === 0) {
+      return true;
+    }
+    return legend.startValue === sortedLegends[index - 1].endValue;
+  });
 }
