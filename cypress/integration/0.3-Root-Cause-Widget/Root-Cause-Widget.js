3c20d056c94dfa7702d1258bf8bc6e14310fbb37
Gift <nnkogift@hotmail.com>
2022-05-17 12:09:28 +0300

    :white_check_mark: fix(tests): Fixes & Improvements
    - Fixed root cause errors

--- cypress/integration/0.3-Root-Cause-Widget/Root-Cause-Widget.js
+++ cypress/integration/0.3-Root-Cause-Widget/Root-Cause-Widget.js
@@ -38,15 +38,23 @@ And("A form of adding new root cause details appeared", () => {
 When("adding new root cause details in the form appear", () => {
   cy.get("[data-test='bootleneck-root-cause']").click();
   //   cy.get("[data-test='bootleneck-root-cause']").select(0);
-  cy.get('[data-value="gMBbWj0ti27"]').click();
+  cy.get("[data-test='dhis2-uicore-singleselectoption']").first().click();
   cy.get("[data-test='indicator-root-cause']").click();
-  cy.get('[data-test="dhis2-uicore-singleselectoption"]').click();
-  cy.get("[data-test='possible-root-cause-input']").type("Test_possible-root-cause");
-  cy.get("[data-test='possible-root-solution-input']").type("Test-possible-root-cause-solution");
+  cy.get("[data-test=\"dhis2-uicore-singleselectoption\"]").click();
+  cy.get("[data-test='possible-root-cause-input']").type("Test possible root-cause");
+  cy.get("[data-test='possible-root-solution-input']").type("Test possible root-cause-solution");
 });
 And("Save the form with details entered", () => {
   cy.get("[data-test='save-new-root-cause']").click();
 });
 Then("I should be able to view the saved root cause", () => {
   cy.get("[data-test='root-cause-form']").should("be.visible");
+});
+
+
+Then(/^I should be able to see list of all the Root cause component$/, function() {
+  cy.get("[data-test=\"root-cause-table\"]").should("be.visible");
+});
+When(/^I navigate to add new root cause component$/, function() {
+  cy.get("[data-test=\"add-new-root-cause\"]").click();
 });
