173f175b2b66275aca0acfe0fd6c60d0d0cd98ce
gnnko <nnkogift@hotmail.com>
2022-09-28 09:29:14 +0300

    feat(map): Implementation
    - Implemented map component from react ui library

--- src/modules/Intervention/components/SubLevelAnalysis/components/Map/index.tsx
+++ src/modules/Intervention/components/SubLevelAnalysis/components/Map/index.tsx
@@ -1,38 +1,24 @@
-import React from "react";
-import { LayersControl, MapContainer, ScaleControl, TileLayer } from "react-leaflet";
-import BoundaryLayer from "./components/BoundaryLayer";
-import useBoundaryData from "./components/BoundaryLayer/hooks/data";
-import DownloadControl from "./components/DownloadControl";
-import FacilityLayer from "./components/FacilityLayer";
-import ThematicLayer from "./components/ThematicLayer";
+import React, { useMemo } from "react";
+import { Map as CustomMap } from "@hisptz/react-ui";
 import useInterventionConfig from "../../../../../../shared/hooks/useInterventionConfig";
 import CardError from "../../../../../../shared/components/errors/CardError";
 import i18n from "@dhis2/d2-i18n";
+import {  CustomThematicPrimitiveLayer } from "@hisptz/react-ui/build/types/components/Map/components/MapLayer/interfaces";
+import { useRecoilValue } from "recoil";
+import { SubLevelOrgUnit } from "../../state/dimensions";
+import { useParams } from "react-router-dom";
+import { InterventionPeriodState } from "../../../../state/selections";
+import { getOrgUnitSelectionFromOrgUnitList } from "./utils/map";
 
-function MapArea() {
-  const { center, bounds } = useBoundaryData();
-
-  return (
-    <div id="map-container" style={{ height: "100%", width: "100%" }}>
-      <MapContainer bounds={bounds} bounceAtZoomLimits center={center} style={{ height: "100%", width: "100%", minHeight: 500 }} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a href="https://carto.com/attribution">CARTO</a>'
-          url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
-        />
-        <ScaleControl position={"bottomleft"} />
-        <LayersControl position={"topleft"}>
-          <DownloadControl />
-          <BoundaryLayer />
-          <FacilityLayer />
-          <ThematicLayer />
-        </LayersControl>
-      </MapContainer>
-    </div>
-  );
-}
 
 export default function Map() {
+  const { id } = useParams<{ id: string }>();
   const { map } = useInterventionConfig();
+  const periodSelection = useRecoilValue(InterventionPeriodState(id));
+
+  const subLevelOrgUnit = useRecoilValue(SubLevelOrgUnit(id));
+
+  const orgUnitSelection = useMemo(() => getOrgUnitSelectionFromOrgUnitList(subLevelOrgUnit), [subLevelOrgUnit]);
 
   if (!map?.enabled) {
     return (
@@ -42,5 +28,44 @@ export default function Map() {
     );
   }
 
-  return <MapArea />;
+  const { coreLayers } = map ?? {};
+  const thematicLayers: CustomThematicPrimitiveLayer[] = coreLayers.thematicLayers.map(layer => {
+    return {
+      id: layer.indicator,
+      type: layer.type,
+      enabled: layer.enabled,
+      control: {
+        enabled: true,
+        position: "topright"
+      },
+      dataItem: {
+        id: layer.indicator,
+        type: "indicator",
+        displayName: layer.indicator,
+        legendConfig: {
+          scale: 5,
+          colorClass: "YlGn"
+        }
+      }
+    };
+  });
+
+  const facilityLayer = {
+    ...coreLayers.facilityLayer,
+  }
+
+  return <CustomMap
+    legends={{
+      enabled: true,
+      position: "topright",
+      collapsible: false
+    }}
+    orgUnitSelection={orgUnitSelection}
+    thematicLayers={thematicLayers}
+    boundaryLayer={coreLayers.boundaryLayer}
+    pointLayer={coreLayers.facilityLayer}
+    periodSelection={{
+      periods: [periodSelection]
+    }}
+  />;
 }
--- src/modules/Intervention/components/SubLevelAnalysis/components/Map/utils/map.ts
+++ src/modules/Intervention/components/SubLevelAnalysis/components/Map/utils/map.ts
@@ -1,12 +1,13 @@
 import { colors } from "@dhis2/ui";
 import { LeafletMouseEvent } from "leaflet";
-import { filter, find, flatten, head } from "lodash";
+import { filter, find, flatten, head, set } from "lodash";
 import { defaultLegendSet } from "../constants/legendSet";
+import type { OrgUnitSelection } from "@hisptz/dhis2-utils";
 
 export function convertCoordinates([lng, lat]: [number, number]): { lat: number; lng: number } {
   return {
     lat: lat,
-    lng: lng,
+    lng: lng
   };
 }
 
@@ -38,4 +39,30 @@ export function getColorFromLegendSet(legendSet: any, value: number): string {
 export function getLegendCount(legend: any, data: any) {
   const { startValue, endValue } = legend;
   return filter(data, (d: any) => d.data >= startValue && d.data <= endValue).length;
+}
+
+
+export function getOrgUnitSelectionFromOrgUnitList(orgUnits: Array<string>): OrgUnitSelection {
+  const orgUnitSelection: OrgUnitSelection = {};
+
+  if (orgUnits.includes("USER_ORGUNIT")) {
+    set(orgUnitSelection, "userOrgUnit", true);
+
+  }
+  if (orgUnits.includes("USER_ORGUNIT_CHILDREN")) {
+    set(orgUnitSelection, "userSubUnit", true);
+  }
+  if (orgUnits.includes("USER_ORGUNIT_GRANDCHILDREN")) {
+    set(orgUnitSelection, "userSubX2Unit", true);
+  }
+  const levels = orgUnits.filter(ou => ou.match(/LEVEL-/)).map(level => level.replaceAll(/LEVEL-/, ""));
+  const groups = orgUnits.filter(ou => ou.match(/OU_GROUP-/)).map(level => level.replaceAll(/OU_GROUP-/, ""));
+  const orgUnitsIds = orgUnits.filter(ou => ![...levels, ...groups, "USER_ORGUNIT", "USER_ORGUNIT_CHILDREN", "USER_ORGUNIT_GRANDCHILDREN"].includes(ou));
+
+  set(orgUnitSelection, "levels", levels);
+  set(orgUnitSelection, "groups", groups);
+  set(orgUnitSelection, "orgUnits", orgUnitsIds);
+
+  return orgUnitSelection;
+
 }
