8f9216cbc35fba08ff92b35a5b0ae9da0d5b1d96
Gift Nnko <nnkogift@gmail.com>
2025-09-22 20:16:31 +0300

    chore: updated app icons - Upgraded dependencies

--- d2.config.js
+++ d2.config.js
@@ -1,22 +1,26 @@
+/**
+ * @type {import("@dhis2/cli-app-scripts").D2Config}
+ */
 const config = {
-  type: "app",
-  title: "Bottleneck Analysis App",
-  name: "bottleneck-analysis-app",
-  entryPoints: {
-    app: "./src/App.tsx",
-  },
-  namespace: "hisptz-bna",
-  customAuthorities: [
-    "BNA_ADD_INTERVENTION",
-    "BNA_EDIT_INTERVENTION",
-    "BNA_DELETE_INTERVENTION",
-    "BNA_VIEW_INTERVENTIONS",
-    "BNA_ADD_ROOT_CAUSE",
-    "BNA_EDIT_ROOT_CAUSE",
-    "BNA_DELETE_ROOT_CAUSE",
-    "BNA_ADD_ARCHIVE",
-    "BNA_DELETE_ARCHIVE",
-  ],
+	id: "9099e396-ec68-4038-a5c9-e60650f49935",
+	type: "app",
+	title: "Bottleneck Analysis (BNA)",
+	name: "bottleneck-analysis-app",
+	entryPoints: {
+		app: "./src/App.tsx",
+	},
+	namespace: "hisptz-bna",
+	customAuthorities: [
+		"BNA_ADD_INTERVENTION",
+		"BNA_EDIT_INTERVENTION",
+		"BNA_DELETE_INTERVENTION",
+		"BNA_VIEW_INTERVENTIONS",
+		"BNA_ADD_ROOT_CAUSE",
+		"BNA_EDIT_ROOT_CAUSE",
+		"BNA_DELETE_ROOT_CAUSE",
+		"BNA_ADD_ARCHIVE",
+		"BNA_DELETE_ARCHIVE",
+	],
 };
 
 module.exports = config;
--- src/modules/Intervention/components/FiltersArea/index.tsx
+++ src/modules/Intervention/components/FiltersArea/index.tsx
@@ -7,40 +7,41 @@ import { InterventionOrgUnitState, InterventionPeriodState } from "../../state/s
 import { ActiveFilters, FilterActive } from "../Header/components/InterventionHeader/state/filter";
 
 export default function FiltersArea(): React.ReactElement {
-  const { id } = useParams<{ id: string }>();
-  const filterActive = useRecoilValue(FilterActive(id));
-  const activeFilters = useRecoilValue(ActiveFilters(id));
-  const period = useRecoilValue(InterventionPeriodState(id));
-  const orgUnit = useRecoilValue(InterventionOrgUnitState(id));
+	const { id } = useParams<{ id: string }>();
+	const filterActive = useRecoilValue(FilterActive(id));
+	const activeFilters = useRecoilValue(ActiveFilters(id));
+	const period = useRecoilValue(InterventionPeriodState(id));
+	const orgUnit = useRecoilValue(InterventionOrgUnitState(id));
 
-  const onClearFilter = useRecoilCallback(({ reset }) => () => {
-    reset(InterventionOrgUnitState(id));
-    reset(InterventionPeriodState(id));
-    reset(ActiveFilters(id));
-  });
+	const onClearFilter = useRecoilCallback(({ reset }) => () => {
+		reset(InterventionOrgUnitState(id));
+		reset(InterventionPeriodState(id));
+		reset(ActiveFilters(id));
+	});
 
-  if (!filterActive) return <div />;
+	if (!filterActive) return <div />;
 
-  return (
-    <div className="row space-between p-16 w-100 align-center">
-      <div className="row gap">
-        <b>{i18n.t("Active Filters: ")}</b>
-        {activeFilters.period && (
-          <Tag permanent>
-            <b>{`${i18n.t("Period")}: `}</b> {`${period.name}`}
-          </Tag>
-        )}
-        {activeFilters.orgUnit && (
-          <Tag permanent>
-            <b>{`${i18n.t("Organisation Unit")}: `}</b> {`${orgUnit.displayName}`}
-          </Tag>
-        )}
-      </div>
-      <div>
-        <Button onClick={onClearFilter} small>
-          {i18n.t("Clear all filter")}
-        </Button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="row space-between p-16 w-100 align-center">
+			<div className="row gap">
+				<b>{i18n.t("Active Filters")}: </b>
+				{activeFilters.period && (
+					<Tag permanent>
+						<b>{`${i18n.t("Period")}: `}</b> {`${period.name}`}
+					</Tag>
+				)}
+				{activeFilters.orgUnit && (
+					<Tag permanent>
+						<b>{`${i18n.t("Organisation Unit")}: `}</b>{" "}
+						{`${orgUnit.displayName}`}
+					</Tag>
+				)}
+			</div>
+			<div>
+				<Button onClick={onClearFilter} small>
+					{i18n.t("Clear all filter")}
+				</Button>
+			</div>
+		</div>
+	);
 }
